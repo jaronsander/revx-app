b3941ffbcdf4873c0854d16b9b9a61c17fae4548
jaronsander <jar.sand.on@gmail.com>
2024-05-02 16:44:33 -0700

    fixed prettier rules

--- pages/upload/index.js
+++ pages/upload/index.js
@@ -10,27 +10,31 @@ const Upload = () => {
 
   const handleUpload = async () => {
     if (!file) {
-        alert('Please select a file first!');
-        return;
+      alert('Please select a file first!');
+      return;
     }
     const formData = new FormData();
     formData.append('file', file);
 
     try {
-        const response = await fetch('http://localhost:7071/api/upload_contacts_csv', { // This should be the URL to your Azure Function
-            method: 'POST',
-            body: formData,
-        });
-        console.log(response);
-        if (response.ok) {
-            console.log('File uploaded successfully');
-            // alert('File uploaded successfully');
-        } else {
-            throw new Error('File upload failed');
+      const response = await fetch(
+        'http://localhost:7071/api/upload_contacts_csv',
+        {
+          // This should be the URL to your Azure Function
+          method: 'POST',
+          body: formData,
         }
+      );
+      console.log(response);
+      if (response.ok) {
+        console.log('File uploaded successfully');
+        // alert('File uploaded successfully');
+      } else {
+        throw new Error('File upload failed');
+      }
     } catch (error) {
-        console.error('Error uploading file:', error);
-        // alert('Error uploading file: ' + error.message);
+      console.error('Error uploading file:', error);
+      // alert('Error uploading file: ' + error.message);
     }
   };
   return (
